3147c57aa2725591f99b6e19e3a909de124ca41a
churuxu <churuxu@126.com>
2016-05-03 14:20:24 +0800

    change release name

--- make.bat
+++ make.bat
@@ -3,13 +3,50 @@
 set TAG_VER=%1
 
 if "%TAG_VER%"=="" (
-  echo usage make tag_version
+  echo "usage make tag_version vs_version"
+  echo "  tag_version like 1.7.0"
+  echo "  vs_version like vs2010"
   exit 1
 )
 
 set DIR_NAME=libuv-%TAG_VER%
 set TAG_NAME=v%TAG_VER%
 set TAG_URL=https://github.com/libuv/libuv/archive/%TAG_NAME%.zip
+
+
+
+if "%2" == "vs2010" goto vs2010
+if "%2" == "vs2012" goto vs2012
+if "%2" == "vs2013" goto vs2013
+if "%2" == "vs2015" goto vs2015
+goto build
+
+:vs2015
+call "%VS140COMNTOOLS%\..\..\vc\vcvarsall.bat"
+if defined VCINSTALLDIR goto build
+echo not installed vs2015
+exit 1
+
+:vs2013
+call "%VS120COMNTOOLS%\..\..\vc\vcvarsall.bat"
+if defined VCINSTALLDIR goto build
+echo not installed vs2013
+exit 1
+
+:vs2012
+call "%VS110COMNTOOLS%\..\..\vc\vcvarsall.bat"
+if defined VCINSTALLDIR goto build
+echo not installed vs2012
+exit 1
+
+:vs2010
+call "%VS100COMNTOOLS%\..\..\vc\vcvarsall.bat"
+if defined VCINSTALLDIR goto build
+echo not installed vs2010
+exit 1
+
+
+:build
 
 if not exist %TAG_NAME%.zip (
   echo download source ...
@@ -38,6 +75,9 @@ if not exist %DIR_NAME%\build\gyp (
 )
 
 
-%VS100COMNTOOLS%\..\..\vc\vcvarsall.bat
-
+"%DIR_NAME%\vcbuild.bat" x86 shared debug || exit 1
 "%DIR_NAME%\vcbuild.bat" x86 shared release || exit 1
+
+
+
+
