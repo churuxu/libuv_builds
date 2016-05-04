610c011290e0ed4774efbd5d548d7fa0e24b6d19
churuxu <churuxu@126.com>
2016-05-04 10:01:50 +0800

    use matrix

--- win32-make.bat
+++ win32-make.bat
@@ -3,9 +3,9 @@
 set TAG_VER=%1
 
 if "%TAG_VER%"=="" (
-  echo "usage make tag_version vs_version"
+  echo "usage make tag_version vc_version"
   echo "  tag_version like 1.7.0"
-  echo "  vs_version like vs2010"
+  echo "  vs_version like vc100"
   exit 1
 )
 
@@ -15,10 +15,10 @@ set TAG_URL=https://github.com/libuv/libuv/archive/%TAG_NAME%.zip
 
 
 
-if "%2" == "vs2010" goto vs2010
-if "%2" == "vs2012" goto vs2012
-if "%2" == "vs2013" goto vs2013
-if "%2" == "vs2015" goto vs2015
+if "%2" == "vc100" goto vs2010
+if "%2" == "vc110" goto vs2012
+if "%2" == "vc120" goto vs2013
+if "%2" == "vc140" goto vs2015
 goto build
 
 :vs2015
@@ -81,5 +81,7 @@ if not exist %DIR_NAME%\build\gyp (
 cmd /c "%DIR_NAME%\vcbuild.bat" x86 shared release || exit 1
 cmd /c "%DIR_NAME%\vcbuild.bat" x86 shared debug || exit 1
 
-7z a libuv_win32.zip %DIR_NAME%\Debug\libuv.* %DIR_NAME%\Release\libuv.* %DIR_NAME%\include\* %DIR_NAME%\LICENSE
+set ZIP_NAME=libuv_win32_%2.zip
+if "%2" == "" set ZIP_NAME=libuv_win32.zip
+7z a %ZIP_NAME% %DIR_NAME%\Debug\libuv.* %DIR_NAME%\Release\libuv.* %DIR_NAME%\include\* %DIR_NAME%\LICENSE
 
